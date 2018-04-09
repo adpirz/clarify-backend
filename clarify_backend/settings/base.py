a684ebb7bdec1492a35713bf1f92d135d52e1181
adpirz <ad.pirz@gmail.com>
2018-04-09 16:16:01 -0700

    all base reqs and settings done

--- clarify_backend/settings/base.py
+++ clarify_backend/settings/base.py
@@ -84,10 +84,7 @@ WSGI_APPLICATION = 'clarify_backend.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db('DATABASE_URL', default="postgres:///clarify")
 }
 
 
