8ab14477f3130f8a583120c8bf25736cd0340223
adpirz <ad.pirz@gmail.com>
2018-04-10 15:15:51 -0700

    looks like AUTH_USER_MODEL may not need to be specified, commenting out to try and make it work

--- clarify_backend/settings/base.py
+++ clarify_backend/settings/base.py
@@ -88,7 +88,7 @@ DATABASES = {
 }
 
 # in case we customizer user model; makes for easy Staff model import
-AUTH_USER_MODEL = 'django.contrib.auth.User'
+# AUTH_USER_MODEL = 'django.contrib.auth.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
