42c59863e88b7f010518ee00539513271b14017e
Tristan McCormick <tristanjmccormick@gmail.com>
2018-04-09 22:54:46 -0400

    Add mock api response for students, sections, etc

--- api/urls.py
+++ api/urls.py
@@ -5,8 +5,12 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from .views import TestView
+from .views import TestView, StudentView, SectionView, GradeLevelView, SchoolView
 
 urlpatterns = [
     url(r'^test', TestView, name='test'),
+    url(r'^student/', StudentView, name='test'),
+    url(r'^section/', SectionView, name='test'),
+    url(r'^grade-level/', GradeLevelView, name='test'),
+    url(r'^school/', SchoolView, name='test'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,82 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from mimesis import Person
 
 # Create your views here.
 
 def TestView(request):
-    return JsonResponse({'data': "Hello World"})
+    return JsonResponse({
+        'data': 'Hello world'
+    })
+
+def StudentView(request):
+    person = Person('en')
+    return JsonResponse({
+        'data': [{"id": i, "name": person.full_name()} for i in range(0,25)]
+    })
+
+def SectionView(request):
+    return JsonResponse({
+        'data': [
+            {
+                'name': 'Community 101',
+                'id': '2341',
+            },
+            {
+                'name': 'Back to the Future 102',
+                'id': '236',
+            },
+            {
+                'name': 'Harry Potter 101',
+                'id': '4685',
+            },
+            {
+                'name': 'Game of Thrones 101',
+                'id': '1508',
+            },
+        ]
+    })
+
+def GradeLevelView(request):
+    return JsonResponse({
+        'data': [
+            {
+                'name': 'Community Course',
+                'id': '2341',
+            },
+            {
+                'name': 'Back to the Future',
+                'id': '236',
+            },
+            {
+                'name': 'Harry Potter',
+                'id': '4685',
+            },
+            {
+                'name': 'Game of Thrones',
+                'id': '1508',
+            },
+        ]
+    })
+
+def SchoolView(request):
+    return JsonResponse({
+        'data': [
+            {
+                'name': 'Central High School',
+                'id': '2341',
+            },
+            {
+                'name': 'Saint Paul Academy',
+                'id': '236',
+            },
+            {
+                'name': 'Archbishop Mitty',
+                'id': '4685',
+            },
+            {
+                'name': 'Richard Montgomery High School',
+                'id': '1508',
+            },
+        ]
+    })
