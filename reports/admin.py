bac6eea5e330ca795650b3afe88b266f0ee0f29a
Tristan McCormick <tristanjmccormick@gmail.com>
2018-07-24 10:36:19 -0400

    Allow superusers to sign in as users from admin

--- reports/admin.py
+++ reports/admin.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import login
+from django.contrib.auth.models import User
 from django.contrib import admin
 from .models import Report, ReportShare
+from django.contrib import messages
 
 
 @admin.register(Report)
@@ -20,3 +23,19 @@ class ReportShareAdmin(admin.ModelAdmin):
     search_fields = ['parent__id', 'parent__query', 'child__id', 'child__query',
                      'parent__staff', 'child__staff']
     list_display = ('parent', 'child')
+
+class UserAdmin(admin.ModelAdmin):
+    def sign_in_as_user(modeladmin, request, queryset):
+        if len(queryset) > 1:
+            messages.error(request, "You cannot sign in as more than one user.")
+            return
+        elif not request.user.is_superuser:
+            messages.error(request, "You have no power here.")
+            return
+        else:
+            login(request, queryset[0])
+
+    actions = [sign_in_as_user]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
