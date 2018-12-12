6048631e15d4521655aea843c927050874affc0f
adpirz <ad.pirz@gmail.com>
2018-12-11 18:44:18 -0800

    sendgrid added; first pass at PasswordResetView

--- clarify_backend/utils.py
+++ clarify_backend/utils.py
@@ -6,9 +6,13 @@ from django.db import models, IntegrityError
 from django.utils import timezone
 from django.conf import settings
 from django.contrib.auth.models import User
+from sendgrid import Email
+from sendgrid.helpers.mail import Content, Mail
 
-from clarify.models import (UserProfile, Section, Student, EnrollmentRecord,
-StaffSectionRecord, Site, Term)
+from clarify.models import (
+    UserProfile, Section, Student, EnrollmentRecord,
+    StaffSectionRecord, Site, Term
+)
 
 
 def try_bulk_or_skip_errors(model, instance_list):
@@ -139,3 +143,30 @@ def manually_roster_with_file(source_file, **kwargs):
                                 user_profile=new_teacher_user_profile,
                                 section=section,
                                 active=True)
+
+def word_hash(length=4):
+    species = sample(WORDS["species"], 1)[0].replace(' ', '')
+    words = "".join([sample(WORDS["words"], 1)[0].lower().capitalize()
+                    for _ in range(length - 1)])
+
+    return species + words
+
+
+def build_reset_email(request, profile: UserProfile):
+    if not profile.reset_token:
+        raise AttributeError("No reset token found")
+    name = profile.get_full_name()
+    reset_token = profile.reset_token
+    domain = request.META['HTTP_HOST']
+    protocol = 'https' if request.is_secure() else 'http'
+
+    from_email = Email("noreply@clarify.school")
+    to_email = Email(profile.user.email)
+    subject = f"Reset password for {name}"
+    body = "To reset your password, please click the link below.\n\n" \
+           f"{protocol}://{domain}/password-reset/?token={reset_token}"
+
+    content = Content("text/plain",
+                      body)
+
+    return Mail(from_email, subject, to_email, content)
