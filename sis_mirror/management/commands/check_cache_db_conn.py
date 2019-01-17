b63c1bbdb482cedb9b2930c14e472cc52bda6b4d
adpirz <ad.pirz@gmail.com>
2019-01-17 05:28:41 -0800

    fixed error print bug, added helpful error info

--- sis_mirror/management/commands/check_cache_db_conn.py
+++ sis_mirror/management/commands/check_cache_db_conn.py
@@ -1,21 +1,25 @@
 from django.core.management.base import BaseCommand
 from django.db import connections
 from django.db.utils import OperationalError
+from pprint import pformat
+from django.conf import settings
 
 
 class Command(BaseCommand):
     help = "My shiny new management command."
 
     def handle(self, *args, **options):
-        db_conn = connections['clarifycache']
+        db_conn = connections['cache']
         try:
             c = db_conn.cursor()
             connected = True
-        except OperationalError as e:
+        except OperationalError:
             connected = False
         if not connected:
             message = self.style.ERROR(
-                f'Could not connect to cache.\n{e}'
+                f'Could not connect to cache.\n'
+                f'settings.DATABASES["cache"]:\n' +
+                pformat(settings.DATABASES["cache"])
             )
         else:
             message = self.style.SUCCESS(
